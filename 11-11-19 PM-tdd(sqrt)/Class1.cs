d571557ffb0e0493f7fff07404defb5eb9143f9c
AndrewKalushka <andrew.kalushka@gmail.com>
2019-11-11 11:26:04 +0200

    Add project files.

---
+++ 11-11-19 PM-tdd(sqrt)/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_11_19_PM_tdd_sqrt_
+{
+    public class Class1
+    {
+    }
+}
